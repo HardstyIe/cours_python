463b01d538b789481c618c90c958614b48752645
hardstyIe <hardstyle.dd@gmail.com>
2025-10-10 18:36:09 +0200

    exo 28->51 python fait

---
+++ test.py
@@ -0,0 +1,7 @@
+nom_utilisateur = input("Quel est ton nom utilisateur")
+mot_de_passe = input("Quel est ton mot de passe ?")
+
+if nom_utilisateur == "test" and mot_de_passe == "test" :
+    print("le nom d'utilisateur et mot de passe correspondent")
+else:
+    print("les informations ne sont pas correspondantes")
